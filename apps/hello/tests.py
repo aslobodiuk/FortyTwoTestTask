bd7690f7b231f0c321b42e3db8feaa921b6c0c66
Aleksey Slobodiuk <aslobodiuk@gmail.com>
2017-06-27 16:11:50 +0300

    ticket:1 made tests for home view (tests failed)

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+from apps.hello import views
 
-# Create your tests here.
 
+class HomeViewTest(TestCase):
 
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 4)
+    def test_home(self):
+        "test for view"
+        client = Client()
+        response = client.get(reverse(views.home))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
