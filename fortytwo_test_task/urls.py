b3a3f76da631c449de25c922b01b1ffaf41cd795
Aleksey Slobodiuk <aslobodiuk@gmail.com>
2017-06-27 15:30:01 +0300

    ticket:1 made urlpattern changes

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
-from apps.hello import views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from apps.hello import views
 admin.autodiscover()
+
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
