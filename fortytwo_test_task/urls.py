5c816dde61a69488027f6d81c023d8709116744a
Aleksey Slobodiuk <aslobodiuk@gmail.com>
2017-06-27 16:15:09 +0300

    ticket:1 implemented home view (tests pass)

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from apps.hello import views
-from django.views.generic import TemplateView
 admin.autodiscover()
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name="home.html")),
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
