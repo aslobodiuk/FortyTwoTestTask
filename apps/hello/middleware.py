3d31db5dec05b0e4c7236fc054bcacae58decef9
Aleksey Slobodiuk <aslobodiuk@gmail.com>
2017-06-29 13:41:50 +0300

    ticket:3 fix middleware

--- apps/hello/middleware.py
+++ apps/hello/middleware.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 class StoreRequest(object):
     def process_request(self, request):
-        if request.is_ajax():
+        path = request.get_full_path()
+        if request.is_ajax() or path[0:8] == '/static/':
             return None
 
-        r = Request(link=request.get_full_path(), time=datetime.now())
+        r = Request(link=path, time=datetime.now())
         r.save()
