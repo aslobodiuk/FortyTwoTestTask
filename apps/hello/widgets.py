08772f6ae46466fcff30cf567c4c2df1b1e7a27f
Aleksey Slobodiuk <aslobodiuk@gmail.com>
2017-07-03 16:43:04 +0300

    ticket:5 code django widget

---
+++ apps/hello/widgets.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.utils.safestring import mark_safe
+
+
+class DatePickerWidget(forms.DateInput):
+
+    def __init__(self, params='', attrs=None):
+        self.params = params
+        super(DatePickerWidget, self).__init__(attrs=attrs)
+
+    def render(self, name, value, attrs=None):
+        rendered = super(DatePickerWidget, self).render(name, value, attrs=attrs)
+        return rendered + mark_safe(
+            u'''<script type="text/javascript">$('#id_%s').datepicker({%s});
+            </script>''' % (name, self.params,))
