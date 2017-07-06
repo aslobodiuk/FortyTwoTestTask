4a491428364589ee43c1a7165c68803011881c36
Aleksey Slobodiuk <aslobodiuk@gmail.com>
2017-07-06 15:51:02 +0300

    ticket:5 fixed flake8

--- apps/hello/widgets.py
+++ apps/hello/widgets.py
@@ -5,7 +5,9 @@ from django.utils.safestring import mark_safe
 class DatePickerWidget(forms.DateInput):
     class Media:
         css = {
-            'all': ("https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css",)
+            'all': (
+                "https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css",
+                )
         }
         js = (
             "https://code.jquery.com/ui/1.12.1/jquery-ui.js",
