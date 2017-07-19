6a127a26b32f652b48f9c186a52176ad909a7350
Aleksey Slobodiuk <aslobodiuk@gmail.com>
2017-07-19 13:39:59 +0300

    ticket:14 add sorting with tablesorter

--- assets/js/all_requests.js
+++ assets/js/all_requests.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
     $form = $('#request_form')
+    $("#myTable").tablesorter({
+    	theme : 'blue',
+    	sortReset: true,
+    }); 
     $('input').change(function(){
         $form.ajaxSubmit();
+        if ($(this).closest('td').prev('td').text() === "True"){
+        	$(this).closest('td').prev('td').text("False");
+        }
+        else{
+        	$(this).closest('td').prev('td').text("True");
+        }
+        var resort = "",
+        	callback = function(table){};
+        $("#myTable").trigger("updateAll", [ resort, callback ]);
     });
 });
