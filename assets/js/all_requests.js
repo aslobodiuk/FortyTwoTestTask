1025a221048570a71078ec83d55e949cda3d458a
Aleksey Slobodiuk <aslobodiuk@gmail.com>
2017-07-19 14:33:39 +0300

    ticket:14 add priority filter via tablesorter

--- assets/js/all_requests.js
+++ assets/js/all_requests.js
@@ -1,19 +1,37 @@
 $(document).ready(function(){
-    $form = $('#request_form')
+    
+    $form = $('#request_form');
+
     $("#myTable").tablesorter({
-    	theme : 'blue',
-    	sortReset: true,
-    }); 
+        theme : 'blue',
+        sortReset: true,
+        widgets: ["zebra", "filter"],
+        widgetOptions : {
+            filter_columnFilters : false,
+            filter_reset : 'button.reset',
+        }
+    });
+
+    $('button[data-filter-column]').click(function(){
+        var filters = [],
+            $t = $(this),
+            col = $t.data('filter-column'),
+            txt = $t.data('filter-text') || $t.text();
+
+        filters[col] = txt;
+        $.tablesorter.setFilters( $('#myTable'), filters, true );
+    });
+
     $('input').change(function(){
         $form.ajaxSubmit();
         if ($(this).closest('td').prev('td').text() === "True"){
-        	$(this).closest('td').prev('td').text("False");
+            $(this).closest('td').prev('td').text("False");
         }
         else{
-        	$(this).closest('td').prev('td').text("True");
+            $(this).closest('td').prev('td').text("True");
         }
         var resort = "",
-        	callback = function(table){};
+            callback = function(table){};
         $("#myTable").trigger("updateAll", [ resort, callback ]);
     });
 });
